6d93d6f225c474b1b63c6e71ddcf666137e3ef6a
Tiago Looze <tiago.looze28@gmail.com>
2025-08-15 02:41:14 -0300

    feat(models): definir modelos SQLAlchemy para usuários, cobranças e transações

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.core.database import Base
@@ -11,7 +11,7 @@ class User(Base):
     cpf = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    balance = Column(Float, default=0.0)
+    balance = Column(Numeric(12, 2), default=0.0)
 
     charges_originating = relationship("Charge", foreign_keys="[Charge.originator_id]", back_populates="originator")
     charges_destined = relationship("Charge", foreign_keys="[Charge.recipient_id]", back_populates="recipient")
@@ -21,10 +21,10 @@ class Charge(Base):
     __tablename__ = "charges"
 
     id = Column(Integer, primary_key=True, index=True)
-    value = Column(Float, nullable=False)
+    value = Column(Numeric(12, 2), nullable=False)
     description = Column(String, nullable=True)
-    status = Column(String, default="Pendente") # Pendente, Paga, Cancelada [cite: 32]
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    status = Column(String, default="Pendente")
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     originator_id = Column(Integer, ForeignKey("users.id"))
     recipient_id = Column(Integer, ForeignKey("users.id"))
@@ -37,10 +37,10 @@ class Transaction(Base):
     __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True, index=True)
-    type = Column(String, nullable=False) # Ex: 'pagamento_saldo', 'pagamento_cartao', 'deposito'
-    amount = Column(Float, nullable=False)
-    status = Column(String, default="Pendente") # Ex: 'aprovada', 'rejeitada'
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    type = Column(String, nullable=False)
+    amount = Column(Numeric(12, 2), nullable=False)
+    status = Column(String, default="Pendente")
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     user_id = Column(Integer, ForeignKey("users.id"))
     charge_id = Column(Integer, ForeignKey("charges.id"), nullable=True)
