583a61f6efeb21e69c07c7e7ed5093c903ac6aa6
Tiago Looze <tiago.looze28@gmail.com>
2025-08-15 02:41:04 -0300

    feat(users): criar endpoints para cadastro e login de usuários

--- app/routers/users.py
+++ app/routers/users.py
@@ -7,6 +7,7 @@ from app.auth import auth
 from app.core import database
 from app.schemas import schemas
 from app.crud import crud
+
 router = APIRouter(
     prefix="/users",
     tags=["users"]
@@ -15,32 +16,35 @@ router = APIRouter(
 # Endpoint de cadastro de usuário
 @router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
-    db_user_cpf = crud.get_user_by_cpf(db, cpf=user.cpf)
-    if db_user_cpf:
+    if crud.get_user_by_cpf(db, cpf=user.cpf):
         raise HTTPException(status_code=400, detail="CPF já registrado")
     
-    db_user_email = crud.get_user_by_email(db, email=user.email)
-    if db_user_email:
+    if crud.get_user_by_email(db, email=user.email):
         raise HTTPException(status_code=400, detail="E-mail já registrado")
     
     return crud.create_user(db=db, user=user)
 
-# Endpoint de login, que retorna um token de autenticação 
+# Endpoint de login, que retorna um token de autenticação
 @router.post("/token", response_model=schemas.Token)
 def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Session = Depends(database.get_db)
 ):
-    # O login deve permitir autenticação por CPF e senha ou e-mail e senha.
+    # Permite login por CPF ou e-mail
     user = crud.get_user_by_email(db, email=form_data.username) or crud.get_user_by_cpf(db, cpf=form_data.username)
+    
     if not user or not auth.verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Credenciais incorretas",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+   
+    login_identifier = user.email if user.email else user.cpf
     access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = auth.create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
+        data={"sub": login_identifier}, expires_delta=access_token_expires
     )
+
     return {"access_token": access_token, "token_type": "bearer"}
